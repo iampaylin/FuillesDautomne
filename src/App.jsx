1e63c9bdc6f094db1207c52d66bd3f75881592e1
Paulo Cesar <itspaylin@gmail.com>
2023-10-19 14:21:28 -0300

    Fuilles D'automne

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Fuilles from './components/Fuilles'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Fuilles />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Fuilles.jsx
@@ -0,0 +1,18 @@
+import './Fuilles.css'
+
+import Outono from '../assets/outono.webp'
+
+const Fuilles = () => {
+    return (
+        <>
+            <main>
+                <a href="https://open.spotify.com/playlist/6AVbMnnjaU8cLEUTcyT9sa?si=ae7a304b639a4318">
+                    <img src={Outono} alt="Outono" />
+                </a>
+                <h1>FUILLES D'AUTOMNE</h1>
+            </main>
+        </>
+    )
+}
+
+export default Fuilles
